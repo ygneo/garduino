b11c93be4ca99e7e40de83cdeecd8d4fe668158b
Martin Fuentes <mfuentes@mfuentes-laptop.(none)>
2012-10-05 10:42:38 +0200

    Fix missing variable declaration and sending both measures to the serial output

--- garduino_two_reles/garduino_two_reles.ino
+++ garduino_two_reles/garduino_two_reles.ino
@@ -3,17 +3,19 @@
  */
 
 const int analogInPin1 = A0; // Analog input pin to the potentiometer #1
-const int analogInPin2 = A1; // Analog input pin to the potentiometer #2
-const int digitalOutPin1 = 1; // Rele-Electrovalve output #1
-const int digitalOutPin2 = 2; // Rele-Electrovalve output #2
+const int analogInPin2 = A2; // Analog input pin to the potentiometer #2
+const int digitalOutPin1 = 2; // Rele-Electrovalve output #1
+const int digitalOutPin2 = 3; // Rele-Electrovalve output #2
 const int minSensorValue1 = 400; // Minimun value from the potentiometer #1 to trigger watering
 const int minSensorValue2 = 500; // Minimun value from the potentiometer #2 to trigger watering
 
 int sensorValue = 0; // value read from the potentiometer
 
-void SendToSerial (int sensorValue)
+void SendToSerial (int number, int sensorValue)
 {
   // print the results to the serial monitor:
+  Serial.print("#");
+  Serial.print(number);
   Serial.print("#");
   Serial.print(sensorValue);
   Serial.print("#");
@@ -28,10 +30,12 @@ void setup() {
 }
 
 void loop() {
+  int sensorValue1, sensorValue2 = 0;
   // read the analog in value
   sensorValue1 = analogRead(analogInPin1);
+  SendToSerial(1, sensorValue1);
   sensorValue2 = analogRead(analogInPin2);
-  SendToSerial(sensorValue);
+  SendToSerial(2, sensorValue2);
   
   if (sensorValue1 <= minSensorValue1) {
      digitalWrite(digitalOutPin1, HIGH); 
