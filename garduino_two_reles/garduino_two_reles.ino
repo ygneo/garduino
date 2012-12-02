37bff2e762ebd7e5f246fe5a04f593449c722857
Antonio Barcia García <antonio.barcia@gmail.com>
2012-12-02 19:12:43 +0100

    Improved watering system. - It will water for a defined amount of time. - It will not check for watering again until a defined interval has passed.

--- garduino_two_reles/garduino_two_reles.ino
+++ garduino_two_reles/garduino_two_reles.ino
@@ -1,17 +1,20 @@
 /*
  Two-entries moisture sensure and reactive watering prototype. 
  */
-
-const int analogInPin1 = A0; // Analog input pin to the potentiometer #1
-const int analogInPin2 = A2; // Analog input pin to the potentiometer #2
+const int analogInPin1 = A0; // Analog input pin from moisture sensor #1
+const int analogInPin2 = A2; // Analog input pin from moisture sensor #2
 const int digitalOutPin1 = 2; // Rele-Electrovalve output #1
 const int digitalOutPin2 = 3; // Rele-Electrovalve output #2
 const int minSensorValue1 = 400; // Minimun value from the potentiometer #1 to trigger watering
 const int minSensorValue2 = 500; // Minimun value from the potentiometer #2 to trigger watering
+const int wateringTime[] = {3000, 6000}; // Watering time for every plant
+const int checkingDelay = 1000; // Delay between checks (for the analog-to-digital converter to settle after last reading)
+const int checkAfterWateringInterval = 300000; // 5 min, for testing
+unsigned long int lastCheck[] = {0, 0}; // Last time moisture sensors was checked
+
 
 void sendToSerial (int number, int sensorValue)
 {
-  // print the results to the serial monitor:
   Serial.print("#");
   Serial.print(number);
   Serial.print("#");
@@ -27,34 +30,45 @@ void sendToSerial (int number, int sensorValue)
 
 void setup() {
   // initialize serial communications at 9600 bps:
-  Serial.begin(9600); 
-  pinMode(digitalOutPin1, OUTPUT);   
-  pinMode(digitalOutPin2, OUTPUT);   
+  Serial.begin(9600);
+  pinMode(digitalOutPin1, OUTPUT);
+  pinMode(digitalOutPin2, OUTPUT);
+  digitalWrite(digitalOutPin1, LOW);
+  digitalWrite(digitalOutPin2, LOW);
 }
 
 void loop() {
   int sensorValue1, sensorValue2;
-  // read the analog in value
+
   sensorValue1 = analogRead(analogInPin1);
   sendToSerial(1, sensorValue1);
+  Serial.print("lc1:\n");
+  Serial.print(lastCheck[0]);
+  Serial.print("\n");
+
   sensorValue2 = analogRead(analogInPin2);
   sendToSerial(2, sensorValue2);
-  
-  if (sensorValue1 <= minSensorValue1) {
+  Serial.print("lc2:\n");
+  Serial.print(lastCheck[1]);
+  Serial.print("\n");
+    
+  if (sensorValue1 <= minSensorValue1 && lastCheck[0] >= checkAfterWateringInterval) {
      digitalWrite(digitalOutPin1, HIGH);
      sendToSerial(1, -1);
-  }
-  else {
+     delay(wateringTime[0]);
      digitalWrite(digitalOutPin1, LOW);
-  }  
-
-  if (sensorValue2 <= minSensorValue2) {
-     digitalWrite(digitalOutPin2, HIGH);
-     sendToSerial(2, -); 
+     lastCheck[0] = 0;
   }
-  else {
+
+  if (sensorValue2 <= minSensorValue2 && lastCheck[1] >= checkAfterWateringInterval) {
+     digitalWrite(digitalOutPin2, HIGH);
+     sendToSerial(2, -1);
+     delay(wateringTime[1]);
      digitalWrite(digitalOutPin2, LOW);
+     lastCheck[1] = 0;
   }
   
-  delay(1000);    
+  delay(checkingDelay);
+  lastCheck[0] += checkingDelay;
+  lastCheck[1] += checkingDelay;
 }
